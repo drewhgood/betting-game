8eb6799a90299be313ff1e98b3950ccbcd3dac35
Andrew Good <drewhgood@gmail.com>
2015-04-06 19:36:29 -0700

    final commit.

--- application.js
+++ application.js
@@ -1,20 +1,32 @@
-$("#start").click(function(){
-    // $('#start').addClass('active');
-    $("#welcome").fadeOut(600, function(){
-      $("#playing").fadeIn(400);
+$( document ).ready(function() {
+
+  $("#start").click(function(){
+      // $('#start').addClass('active');
+      $("#welcome").fadeOut(600, function(){
+        $("#playing").fadeIn(400);
+      });
+  });
+
+  $("#play").click(function(){
+    promptForBet();
+    promptForGuess();
+    evaluate(guess,generateAnswer(randomNumberRange));
+
+    $('#cash').text("$"+playerCash);
+
+    $( "#info" ).animate({height:'72px'}, 500 , function(){
+      $("#scoreboard").fadeIn(400, function(){
+        $('#scoreboard').delay(2200).fadeOut(400);
+        $("#info").delay(2500).animate({height:'0px'}, 500);
+      });
     });
-});
+  });
 
+  $("#restart").click(function(){
+    restartGame();
+    $('#cash').text('Game Reset').delay(1000).queue(function(next) {
+      $(this).text("$"+playerCash);
+    });
+  });
 
-$("#play").click(function(){
-  promptForBet();
-  promptForGuess();
-  console.log('guess '+ guess)
-  console.log('bet '+ bet)
-  evaluate(guess,generateAnswer(randomNumberRange));
-  console.log(playerCash)
-});
-
-$("#restart").click(function(){
-  restartGame();
 });
--- game.js
+++ game.js
@@ -1,54 +1,77 @@
-var startingCash = 100;
-var playerCash = startingCash
-var promptForBetText = 'Please place a bet between $5 and $10.';
-var promptForGuessText = 'Please make a guess between 1 and 10.';
-var randomNumberRange = 10
-var bet;
-var guess;
 
 
-var promptForBet = function(){ 
-  bet = $('#wager').val();
-};
+  var startingCash = 100;
+  var playerCash = startingCash
+  var promptForBetText = 'Please place a bet between $5 and $10.';
+  var promptForGuessText = 'Please make a guess between 1 and 10.';
+  var correctText = 'You were right. Lucky, but right!';
+  var wrongText = "Wrong! We told you... the house always wins";
+  var almostText = "It's a draw, try again.";
+  var randomNumberRange = 10
+  var bet;
+  var guess;
 
-var promptForGuess = function(){ 
-  guess = $('#guess').val();
-};
+  var promptForBet = function(){ 
+    bet = $('#wager').val();
+  };
 
-var generateAnswer = function(n){
-  return Math.floor((Math.random() * n) + 1);
-}
+  var promptForGuess = function(){ 
+    guess = $('#guess').val();
+  };
 
-var evaluate = function(guess, answer){
-  console.log('answer is '+answer)
-  if(guess == answer){
-    updateCash('correct')
-    console.log('correct');
+  var generateAnswer = function(n){
+    return Math.floor((Math.random() * n) + 1);
   }
-  else if (guess == answer + 1 || guess == answer - 1){
-    console.log('keep money')
-  }
-  else{
-    updateCash('wrong')
-    console.log('wrong');
-  }
-};
 
-var updateCash =function(outcome){
-  outcome == 'correct' ? playerCash += bet * 2 : playerCash -= bet
-};
+  var evaluate = function(guess, answer){
+   
+    if(guess == answer){
+      updateCash('correct');
+      updateScoreboard(correctText);
+      highlighCash('correct');
+    }
+    else if (guess == answer + 1 || guess == answer - 1){  
+      updateScoreboard(almostText);
+      highlighCash('almost');
+    }
+    else{
+      updateCash('wrong');
+      updateScoreboard(wrongText);
+      highlighCash('wrong');
+    }
+  };
 
+  var updateCash =function(outcome){
+    outcome == 'correct' ? playerCash += bet * 2 : playerCash -= bet
+  };
 
-var restartGame = function(){
-  playerCash = startingCash;
-  guess = null;
-  bet = null;
-};
+  var restartGame = function(){
+    playerCash = startingCash;
+    guess = null;
+    bet = null;
+  };
 
-  
- 
+  var updateScoreboard = function(text){
+    $('#notice').text(text)
+  };
 
+  var highlighCash = function(outcome){
+    var active;
+    if(outcome === 'correct'){
+      active = 'correct'
+    }
+    else if(outcome === 'wrong'){
+      active ='wrong'
+    }
+    else{
+      active ='almost'
+    }
 
-
+    
+    $('#cash').addClass(active).delay(3300).queue(function(next) {
+      $(this).removeClass();
+    });
+   
+  };
 
 
